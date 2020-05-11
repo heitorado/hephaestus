2d031e5c603a935757b79e84c7f72fdf340ae1f4
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-05-10 23:37:32 -0300

    Add General Styles and Colors

---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+};
---
+++ src/styles/general.js
@@ -0,0 +1,11 @@
+import colors from 'styles/colors';
+import metrics from 'styles/metrics';
+
+export default {
+  box: {
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    padding: metrics.basePadding,
+    margin: metrics.baseMargin / 2,
+  },
+};
