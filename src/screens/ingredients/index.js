89e853643905f5f968ddad1994a006a7ef3c864c
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-05-17 17:22:33 -0300

    Add Flatlist and FAB to ingredients screen

--- src/screens/ingredients/index.js
+++ src/screens/ingredients/index.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from './styles';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import ListItem from 'components/list_item/index';
+import FloatingActionButton from 'components/floating_action_button/index';
 
-const IngredientsScreen = () => (
-  <View style={styles.container}>
-    <Text>I am the Ingredients screen.</Text>
-  </View>
-);
+
+class IngredientsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: [
+        {
+          id: 1, name: 'Base Glicerinada Branca', price: 19.99, quantity: 1, measure: 'kg', notes: '',
+        },
+        {
+          id: 2, name: 'Mentol', price: 19.99, quantity: 20, measure: 'g', notes: '',
+        },
+        {
+          id: 3, name: 'Álcool de Cereais', price: 19.99, quantity: 1, measure: 'l', notes: '',
+        },
+        {
+          id: 4, name: 'Essência Maçã Madura', price: 19.99, quantity: 1, measure: 'ml', notes: '',
+        },
+        {
+          id: 5, name: 'Fita para Embrulho Vermelha', price: 19.99, quantity: 5, measure: 'm', notes: '',
+        },
+      ],
+    };
+  }
+
+  renderIngredientItem = ({ item }) => <ListItem titleText={item.name} subText={`${item.price}`} />
+
+  render() {
+    const { ingredients } = this.state;
+    return (
+      <View>
+        <FlatList
+          data={ingredients}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={this.renderIngredientItem}
+        />
+        <FloatingActionButton />
+      </View>
+    );
+  }
+}
 
 export default IngredientsScreen;
