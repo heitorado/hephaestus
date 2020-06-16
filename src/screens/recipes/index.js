3890182fcfd1227de2adbfcbf0e5bfc74babaa35
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-16 13:34:55 -0300

    Basic apollo usage for retrieving recipe list

--- src/screens/recipes/index.js
+++ src/screens/recipes/index.js
@@ -1,53 +1,40 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import ListItem from 'components/list_item/index';
 import FloatingActionButton from 'components/floating_action_button/index';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-class RecipesScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: [
-        { id: 1, name: 'Sabonete Glicerinado', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 2, name: 'Vela Aromática', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 3, name: 'Esfera Efervescente', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 4, name: 'Óleo de Banho', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 5, name: 'Sabonete Água de Arroz', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 6, name: 'Sabonete Cold Process', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 7, name: 'Aromatizador de ambiente', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 8, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 9, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 10, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 11, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 12, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 13, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 14, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 15, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 16, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 17, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 18, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 19, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-        { id: 20, name: 'Algum outro produto', description: 'Soap ipsum dolor sit amet conseqtur adpliscing elit' },
-      ],
-    };
+const RECIPES = gql`
+  query {
+    recipes {
+      id
+      name
+      description
+    }
   }
+`;
 
-  renderRecipeItem = ({ item }) => <ListItem titleText={item.name} subText={item.description} />
+const renderRecipeItem = ({ item }) => <ListItem titleText={item.name} subText={item.description} />;
 
-  render() {
-    const { recipes } = this.state;
-    return (
-      <View>
-        <FlatList
-          data={recipes}
-          keyExtractor={(item) => String(item.id)}
-          renderItem={this.renderRecipeItem}
-        />
-        <FloatingActionButton />
-      </View>
-    );
-  }
+export default function RecipesScreen() {
+
+  const { loading, error, data } = useQuery(RECIPES);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error :(</Text>;
+
+  console.log(data.recipes);
+
+  return (
+    <View>
+      <FlatList
+        data={data.recipes}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={renderRecipeItem}
+      />
+      <FloatingActionButton />
+    </View>
+  );
 }
-
-export default RecipesScreen;
