642b3f63413f9371bf8e25d46dfcd46722f65b08
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-16 13:34:35 -0300

    Setup connection for apollo client

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import AppIndex from 'index';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://7394f6d1f3b9.ngrok.io/graphql',
+  }),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <AppIndex />
+    <ApolloProvider client={client}>
+      <AppIndex />
+    </ApolloProvider>
   );
 }
